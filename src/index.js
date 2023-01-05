10d7b6d2be7bef8c1a17953574f9a54b735d4850
Dasha-Vr <dasha-sheludyakova@yandex.ru>
2023-01-05 14:30:52 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    n = n.toString()
+    let result = ''
+    for(let cont = 0; cont < n.length; cont += 1){
+        result = `${n[cont]}${result}`
+    }
+    return +result
 }
